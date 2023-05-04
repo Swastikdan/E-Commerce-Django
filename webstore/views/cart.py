868e6614a28a70e74a41b4d267c7e3dcb98f502d
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2023-05-04 22:29:12 +0530

    cart and  oders changed

--- webstore/views/cart.py
+++ webstore/views/cart.py
@@ -5,10 +5,37 @@ from webstore.models.customer import Customer
 from django.views import  View
 from webstore.models.product import Products
 
+from django.http import JsonResponse
+
+from django.shortcuts import render, redirect
+from django.views import View
+from webstore.models.product import Products
+
 class Cart(View):
-    def get(self , request):
+    def get(self, request):
         ids = list(request.session.get('cart').keys())
         products = Products.get_products_by_id(ids)
-        print(products)
-        return render(request , 'cart.html' , {'products' : products} )
+        return render(request, 'cart.html', {'products': products})
+
+    def post(self, request):
+        product_id = request.POST.get('product')
+        cart = request.session.get('cart')
+        if cart:
+            quantity = cart.get(product_id)
+            if quantity:
+                if request.POST.get('remove'):
+                    if quantity <= 1:
+                        cart.pop(product_id)
+                    else:
+                        cart[product_id] = quantity - 1
+                else:
+                    cart[product_id] = quantity + 1
+            else:
+                cart[product_id] = 1
+        else:
+            cart = {product_id: 1}
+
+        request.session['cart'] = cart
+        return redirect('cart')
+
 
