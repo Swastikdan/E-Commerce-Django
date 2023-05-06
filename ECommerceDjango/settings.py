6ea5f363404b17a52d828be6183c984d4c9bdcca
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2023-05-06 21:41:56 +0530

    some commit

--- ECommerceDjango/settings.py
+++ ECommerceDjango/settings.py
@@ -25,9 +25,9 @@ SECRET_KEY = '-95t%=#4o3@l-(-%ok9*h%n3!0(sdchjn%+_$5#umaj-!3bg*7'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['.vercel.app','localhost']
+ALLOWED_HOSTS = ['*']
 
-CSRF_TRUSTED_ORIGINS = ['http://.vercel.app', 'https://.vercel.app']
+CSRF_TRUSTED_ORIGINS = ['http://.pythonanywhere.com', 'https://.pythonanywhere.com']
 
 
 APPEND_SLASH=False 
--- ECommerceDjango/urls.py
+++ ECommerceDjango/urls.py
@@ -7,4 +7,4 @@ from . import settings
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('' , include('webstore.urls'))
-] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+]+ static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
