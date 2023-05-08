924fd9c396f4d2ea9b5be03be49890689e941235
Swastik Dan <57379785+Swastikdan@users.noreply.github.com>
2023-05-08 19:28:04 +0530

    products added with dummy image

--- webstore/models/product.py
+++ webstore/models/product.py
@@ -6,7 +6,7 @@ class Products(models.Model):
     name = models.CharField(max_length=60)
     price = models.IntegerField(default=0)
     category = models.ForeignKey(Category, on_delete=models.CASCADE, default=1)
-    description = models.CharField(max_length=250, default='', blank=True, null=True)
+    description = models.CharField(max_length=500, default='', blank=True, null=True)
     image = models.ImageField(upload_to='images/')
 
     @staticmethod
